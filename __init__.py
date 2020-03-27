a7b042c39eaaa2c6f037657f747786911bd80663
Linus S <LinusS1@users.noreply.github.com>
2020-03-26 18:30:13 -0700

    Added acrostic passwords

--- __init__.py
+++ __init__.py
@@ -8,14 +8,17 @@ class PasswordCreator(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder("CreatePassword").require("Create").require("Password").optionally("NumberOfWords"))
+    @intent_handler(IntentBuilder("CreatePassword").require("Create").require("Password").optionally("NumberOfWords").optionally("Acrostic"))
     def handle_creator_password(self, message):
         number_of_words = message.data.get("NumberOfWords")
         number_of_words = extract_number(number_of_words) if number_of_words else 6
 
+        acrostic = message.data.get("Acrostic")
+        acrostic = acrostic if acrostic else False
+
         wordfile = xp.locate_wordfile()
         words = xp.generate_wordlist(wordfile=wordfile)  # Can modify
-        password = xp.generate_xkcdpassword(words, number_of_words)  # Can modify
+        password = xp.generate_xkcdpassword(words, number_of_words, acrostic=acrostic)  # Can modify
 
         self.speak_dialog('creator.password', data={
             'password': password
