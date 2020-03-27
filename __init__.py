75625f28a2766927b3e6c800b1c137c03a48e34c
Linus S <LinusS1@users.noreply.github.com>
2020-03-26 18:06:50 -0700

    Added number of words option

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
+from lingua_franca.parse import extract_number
+from adapt.intent import IntentBuilder
 from xkcdpass import xkcd_password as xp
 
 
@@ -6,11 +8,14 @@ class PasswordCreator(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('creator.password.intent')
+    @intent_handler(IntentBuilder("CreatePassword").require("Create").require("Password").optionally("NumberOfWords"))
     def handle_creator_password(self, message):
+        number_of_words = message.data.get("NumberOfWords")
+        number_of_words = extract_number(number_of_words) if number_of_words else 6
+
         wordfile = xp.locate_wordfile()
         words = xp.generate_wordlist(wordfile=wordfile)  # Can modify
-        password = xp.generate_xkcdpassword(words)  # Can modify
+        password = xp.generate_xkcdpassword(words, number_of_words)  # Can modify
 
         self.speak_dialog('creator.password', data={
             'password': password
